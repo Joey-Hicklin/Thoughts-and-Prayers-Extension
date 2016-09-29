3c3c57526ae92f210a9ad74d3ac9574ee646a4a1
JoeyHicklin <j.hicklin87@yahoo.com>
2016-09-28 20:13:49 -0700

    First working version

--- content.js
+++ content.js
@@ -1,4 +1,14 @@
+function randNum(maxNum) {
+    return Math.floor(Math.random() * maxNum);
+}
+
 let pTags = document.getElementsByTagName("p");
+
+for (var i = 0 ; i < pTags.length; i++) {
+	const title = ["lubricated shit-stain", "douche-balloon", "babbling twat-waffle", "kaleidoscope of herepes sores", "torrent of rotting urine", "small, small piece of what is left of Hitler", "CHRISTIAN!!1!!1", "weak, defenseless pit viper", "bloody cum stain from Rudy Giuliani", "useless human being", "scourge on the Earth", "souless shell", "blanket answer for abortion", "parent's worst nightmare", "cause for someone's teardrop tattoo"];
+	let r = randNum(title.length);
+	pTags[i].innerText = pTags[i].innerText.replace(/(my )*(thoughts and prayers |thoughts )(go out to |are with)*/gi, "I am a " + title[r] + " and I only want you to think of me when a tragedy occurs. Oh, and by the way, ");
+}
 port.onMessage.addListener(function(msg){
   alert("response recieved");
 });
