181a569a82424f9cb65c147674f62c563cd7782a
JoeyHicklin <j.hicklin87@yahoo.com>
2016-09-28 23:45:11 -0700

    Link text replacement also working now

--- content.js
+++ content.js
@@ -5,12 +5,15 @@ function randNum(maxNum) {
 $('p').each(function(){
 	const title = ["lubricated shit-stain", "douche-balloon", "babbling twat-waffle", "kaleidoscope of herepes sores", "torrent of rotting urine", "small, small piece of what is left of Hitler", "CHRISTIAN!!1!!1", "weak, defenseless pit viper", "bloody cum stain from Rudy Giuliani", "useless human being", "scourge on the Earth", "souless shell", "blanket answer for abortion", "parent's worst nightmare", "cause for someone's teardrop tattoo"];
 	let r = randNum(title.length);
+	$(this).children().each(function(){
+		let workingLink = $(this).text();
+		let newText = workingLink.replace(/(my )*(thoughts and prayers |thoughts )(go out to |are with)*/gi, "I am a " + title[r] + " and I only want you to think of me when a tragedy occurs. Oh, and by the way, ");
+		$(this).text(newText);
+	});
 	$(this).contents().filter(function(){
 		return this.nodeType == 3;
 	}).each(function(){
-		newText = this.nodeValue.replace(/(my )*(thoughts and prayers |thoughts )(go out to |are with)*/gi, "I am a " + title[r] + " and I only want you to think of me when a tragedy occurs. Oh, and by the way, ");
+		let newText = this.nodeValue.replace(/(my )*(thoughts and prayers |thoughts )(go out to |are with)*/gi, "I am a " + title[r] + " and I only want you to think of me when a tragedy occurs. Oh, and by the way, ");
 		this.nodeValue = newText;
 	});
-	
-	
 })
